4c20fb74e49d85287bca17b2a98e00d5998e66c0
candy01429 <61113675+candy01429@users.noreply.github.com>
2020-11-09 16:18:42 +0800

    Create 201109-JS-function函式.js

---
+++ 201109-JS-function函式.js
@@ -0,0 +1,10 @@
+function square(number){
+    return number*number
+}
+function noReturn(){
+}
+// 可以將回傳值存到一個變數
+var squareValue = square(10);
+console.log(squareValue); // 100
+console.log(square(10)*10); // 1000
+console.log(noReturn()); // undefined
